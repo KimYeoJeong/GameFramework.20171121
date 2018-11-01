5d995d519f62f9b93b44aced8395768d32acbe1d
user <user@532-20>
2018-11-01 18:17:16 +0900

    13완료

---
+++ PP13.KInputHandler/Player.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "GameObject.h"
+#include "SDLGameObject.h"
+#include "InputHandler.h"
+
+class Player : public SDLGameObject
+{
+public:
+	Player(const LoaderParams* pParams);
+	void draw();
+	void update();
+	void clean();
+
+private:
+	void handleInput();
+};
