3eae6a9f686ee5bf4684b8e15318ccf3613749b1
user <user@532-20>
2018-11-06 13:19:30 +0900

    PP14

---
+++ PP14.MInputHandler/InputHandler.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "SDL.h"
+#include "Vector2D.h"
+#include <vector>
+
+enum mouse_buttons
+{
+	LEFT = 0,
+	MIDDLE = 1,
+	RIGHT = 2
+};
+
+class InputHandler
+{
+	const Uint8* m_keystates;
+
+public:
+	static InputHandler* Instance()
+	{
+		if (s_pInstance == 0) {
+			s_pInstance = new InputHandler();
+		}
+		return s_pInstance;
+	}
+
+							
+	std::vector<bool> m_mouseButtonStates;			
+	Vector2D* m_mousePosition;
+
+	void update();
+	void clean();  // 디바이스 관련 초기화된 부분을 clear 
+	bool isKeyDown(SDL_Scancode key);
+
+	void onKeyUp();
+	void onKeyDown();
+	void onMouseMove(SDL_Event& event);
+	void onMouseButtonDown(SDL_Event& event);
+	void onMouseButtonUp(SDL_Event& event);
+
+	bool getMouseButtonState(int buttonNumber);
+	Vector2D* getMousePosition();
+
+private:
+	InputHandler();
+	~InputHandler() {}
+	static InputHandler* s_pInstance;
+};
+
+typedef InputHandler TheInputHandler;
